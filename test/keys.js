c63f35dcf719b45a5190cc8e2f770e34d170dacf
Drew Bollinger <drew@developmentseed.org>
2016-06-07 10:21:27 -0400

    Add test for JSON parsing

--- test/keys.js
+++ test/keys.js
@@ -1,7 +1,18 @@
 var test = require('ava');
 var fs = require('fs');
 
-test('Data exists with five basic keys', function (t) {
+test('All JSON files should parse correctly', function (t) {
+  var blurbs = fs.readFileSync('../app/assets/data/blurbs.json');
+  var oilfields = fs.readFileSync('../app/assets/data/oilfields.geojson');
+  var oils = fs.readFileSync('../app/assets/data/oils.json');
+  var prices = fs.readFileSync('../app/assets/data/prices.json');
+  var related = fs.readFileSync('../app/assets/data/related.json');
+  [ blurbs, oilfields, oils, prices, related ].forEach(function (file) {
+    t.notThrows(function () { JSON.parse(file); });
+  });
+});
+
+test('Data should exist with five basic keys', function (t) {
   var oils = JSON.parse(fs.readFileSync('../app/assets/data/oils.json', 'utf8'));
   t.same(Object.keys(oils), ['info', 'opgee', 'prelim', 'metadata', 'lhv']);
 });
