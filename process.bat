5fbf3a64789e6dbc9b80bee8ce474bc5e86cbb38
Drew Bollinger <drew@developmentseed.org>
2016-10-17 14:22:51 -0400

    Update processing for new data structure

--- process.bat
+++ process.bat
@@ -1,29 +1,19 @@
 FOR %%G IN (*.csv) DO (
-  iconv -f cp1254 -t utf-8 %%G > %%G.temp
+  iconv -f cp1254 -t utf-8 %%G > %%~nG.txt
+)
+
+TYPE info.txt | csvjson |  jq "map({(.Unique): .}) | add " > info.json
+DEL info.txt
+
+FOR %%G IN (opgee_run*.txt) DO (
+  TYPE %%G | csvjson | jq "map({(.Unique): del(.Run)}) | add " > %%~nG.json
   DEL %%G
-  ren %%G.temp %%G
 )
 
-TYPE info.csv | csvjson |  jq "map({(.Unique): .}) | add " > info.json
-
-FOR %%G IN (opgee_run*.csv) DO (
-  TYPE %%G | csvjson | jq "map({(.Unique): .}) | add | { (.[keys[0]].Run): del(.[].Run)}" >> temp_opgee.json
+FOR %%G IN (prelim_run*.txt) DO (
+  TYPE %%G | csvjson | jq "map({(.Unique): del(.Run)}) | add " > %%~nG.json
+  DEL %%G
 )
 
-TYPE temp_opgee.json | jq --slurp "add | { opgee: . } " > opgee.json
-
-DEL temp_opgee.json
-
-FOR %%G IN (prelim_run*.csv) DO (
-  TYPE %%G | csvjson | jq "map({(.Unique): .}) | add | { (.[keys[0]].Run): del(.[].Run)}" >> temp_prelim.json
-)
-
-TYPE temp_prelim.json | jq --slurp "add | { prelim: . }" > prelim.json
-
-DEL temp_prelim.json
-
-TYPE metadata.csv | csvjson | jq ".[]" > metadata.json
-
-TYPE opgee.json prelim.json | jq --slurp "add" > oils.json
-
-DEL opgee.json prelim.json
+TYPE metadata.txt | csvjson | jq ".[]" > metadata.json
+DEL metadata.txt
