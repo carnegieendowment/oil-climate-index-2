3661c0211bd9c8d3a2088ff7d1be148bab55dab6
Drew Bollinger <drew@developmentseed.org>
2016-10-17 14:34:48 -0400

    Update precalc for new data structure

--- .build_scripts/precalc.js
+++ .build_scripts/precalc.js
@@ -2,10 +2,38 @@ var fs = require('fs');
 var utils = require('./utils');
 
 global.Oci = {};
-Oci.data = JSON.parse(fs.readFileSync('app/assets/data/oils.json'))
+Oci.data = {
+  opgee: {},
+  prelim: {}
+}
+Oci.data.metadata = JSON.parse(fs.readFileSync('app/assets/data/metadata.json'))
 Oci.prices = JSON.parse(fs.readFileSync('app/assets/data/prices.json'))
 Oci.data.info = JSON.parse(fs.readFileSync('app/assets/data/info.json'))
 Oci.data.globalExtents = {}
+
+// Load all data based on metadata
+var metadata = Oci.data.metadata
+var si = utils.trimMetadataArray(metadata.solarSteam.split(','));
+var wi = utils.trimMetadataArray(metadata.water.split(','));
+var fi = utils.trimMetadataArray(metadata.flare.split(','));
+var ri = utils.trimMetadataArray(metadata.refinery.split(','));
+var li = [1, 0];
+
+si.forEach(function (_, s) {
+  wi.forEach(function (_, w) {
+    fi.forEach(function (_, f) {
+      var temp = JSON.parse(fs.readFileSync('app/assets/data/opgee/opgee_run' + s + w + f + '.json'))
+      Oci.data.opgee['run' + s + w + f] = temp
+    })
+  })
+})
+
+ri.forEach(function (_, r) {
+  li.forEach(function (_, l) {
+      var temp = JSON.parse(fs.readFileSync('app/assets/data/prelim/prelim_run' + r + l + '.json'))
+      Oci.data.prelim['run' + r + l] = temp
+  })
+})
 
 var ratios = ['perBarrel', 'perMJ', 'perCurrent', 'perHistoric', 'perDollar'];
 var minMaxes = ['min', 'max'];
